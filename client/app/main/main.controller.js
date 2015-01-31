ed08293093c55a1cf05eded5778451451fddefc5
MARCUS HSU <mh3346@columbia.edu>
2015-01-31 14:25:00 -0500

    Added layout for login and signup

--- client/app/main/main.controller.js
+++ client/app/main/main.controller.js
@@ -1,27 +1,77 @@
 'use strict';
 
-angular.module('firstAppApp')
-  .controller('MainCtrl', function ($scope, $http, socket) {
+angular.module('firstAppApp').controller('MainCtrl', function ($scope, $http, $modal, socket) {
     $scope.awesomeThings = [];
 
-    $http.get('/api/things').success(function(awesomeThings) {
-      $scope.awesomeThings = awesomeThings;
-      socket.syncUpdates('thing', $scope.awesomeThings);
+    $http.get('/api/things').success(function (awesomeThings) {
+        $scope.awesomeThings = awesomeThings;
+        socket.syncUpdates('thing', $scope.awesomeThings);
     });
 
-    $scope.addThing = function() {
-      if($scope.newThing === '') {
-        return;
-      }
-      $http.post('/api/things', { name: $scope.newThing });
-      $scope.newThing = '';
+    $scope.addThing = function () {
+        if ($scope.newThing === '') {
+            return;
+        }
+        $http.post('/api/things', { name: $scope.newThing });
+        $scope.newThing = '';
     };
 
-    $scope.deleteThing = function(thing) {
-      $http.delete('/api/things/' + thing._id);
+    $scope.deleteThing = function (thing) {
+        $http.delete('/api/things/' + thing._id);
     };
 
     $scope.$on('$destroy', function () {
-      socket.unsyncUpdates('thing');
+        socket.unsyncUpdates('thing');
     });
-  });
+
+    $scope.signupModal = function () {
+        var modalInstance = $modal.open({
+                                            templateUrl: "/components/modal/modal.signup.html",
+                                            size: "md",
+                                            controller: SignupModalCtrl
+                                        });
+        modalInstance.result.then(function (ctrl) {
+            console.log(ctrl)
+
+            if (ctrl) {
+                var data = new FormData();
+                data.append("userfilename", ctrl.firstname);
+                data.append("userlastname", ctrl.lastname);
+                data.append("email", ctrl.email);
+                data.append("password", ctrl.password);
+                data.append("confirmpassword", ctrl.confirmpassword);
+                data.append("phonenumber", ctrl.phonenumber);
+                data.append("userprofilepicture", $("#UploadPhoto")[0].files[0]);
+                $.ajax({
+                           url: "/api/document/template",
+                           data: data,
+                           cache: false,
+                           contentType: false,
+                           processData: false,
+                           type: "POST",
+                           success: function (data) {
+                               console.log(data);
+
+                           }
+                       });
+            }
+        });
+    }
+    var SignupModalCtrl = function ($scope, $modalInstance) {
+        $scope.signup = function () {
+            $modalInstance.close({
+                                     firstname: $scope.FirstName,
+                                     lastname: $scope.LastName,
+                                     email: $scope.Email,
+                                     password: $scope.Password,
+                                     confirmpassword: $scope.ConfirmPassword,
+                                     phonenumber: $scope.PhoneNumber
+                                 });
+        };
+        $scope.cancel = function () {
+            $modalInstance.close(false);
+        };
+    };
+
+})
+;
